4accd21b87cb8a2024899e7582783ec170835e0a
Caleb King <caleb.king536@gmail.com>
2019-11-12 14:18:41 -0700

    progress on Participants.js

---
+++ src/Participant.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { stat } from 'fs';
+
+
+
+export default function participants(props) {
+  
+  let statusIndicator = 'status dot ';
+  if (props.inSession) {
+    statusIndicator += 'green';
+  } else {
+    statusIndicator += 'grey';
+  }
+  console.log(statusIndicator);
+
+  let statusMessage = '';
+  if (props.inSession && props.onStage) {
+    statusMessage = 'on stage';
+  } 
+  else if (props.inSession && !props.onStage) {
+    statusMessage = 'in session';
+  } 
+  else {
+    statusMessage = 'left session';
+  }
+  console.log(statusMessage);
+
+
+  return (
+    <div className='participants'>
+        <img className='user-avatar' src={props.avatar} alt="user avatar"/>
+        <div className='info'>
+            <h6 className="name">{props.name}</h6>
+            <div className={statusIndicator}>{statusMessage}</div>
+        </div>
+    </div>
+  )
+}
---
+++ src/ParticipantList.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import Participant from './Participant.js'
+import './ParticipantList.css';
+
